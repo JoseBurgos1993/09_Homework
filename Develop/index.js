7f483c22ba194199eaa5677a50df6e9043ea8fb7
JoseBurgos1993 <burgosjd1993@gmail.com>
2020-07-15 11:58:42 -0400

    Small change

--- Develop/index.js
+++ Develop/index.js
@@ -1,15 +1,24 @@
 // array of questions for user
 const questions = [
-
+    "What is the title of your project? ",
+    "Write a description for this project: ",
+    "Write a table of contents: ",
+    "Write installation instructions: ",
+    "Write project usage: ",
+    "Write a license for the project: ",
+    "Write constributing: ",
+    "Write tests: ",
+    "Write questions: "
 ];
 
 // function to write README file
 function writeToFile(fileName, data) {
+
 }
 
 // function to initialize program
 function init() {
-
+    console.log(questions);
 }
 
 // function call to initialize program
